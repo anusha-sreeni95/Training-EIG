2e57d3cc5019f1ba5a63839257d2897fcf3a6769
hemant.v <hemant.v@endurance.com>
2017-06-30 16:22:36 +0530

    Initial commit

---
+++ src/main/java/com/endurance/products/teamtraining/crudapi/CrudapiApplication.java
@@ -0,0 +1,12 @@
+package com.endurance.products.teamtraining.crudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudapiApplication.class, args);
+	}
+}
