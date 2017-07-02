f208ea0dc9027604f39bea8cb855cddaac36a0b7
anusha.sr <anusha.sr@endurance.com>
2017-07-02 18:55:10 +0530

    test not working

---
+++ src/test/java/com/endurance/products/teamtraining/crudapi/EmployeeServiceTest.java
@@ -0,0 +1,221 @@
+package com.endurance.products.teamtraining.crudapi;
+
+import com.endurance.products.teamtraining.crudapi.auth.Employee;
+import com.endurance.products.teamtraining.crudapi.crud.EmployeeService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Created by anusha on 2/7/17.
+ */
+public class EmployeeServiceTest {
+    HttpServletResponse response = new HttpServletResponse() {
+        @Override
+        public void addCookie(Cookie cookie) {
+
+        }
+
+        @Override
+        public boolean containsHeader(String s) {
+            return false;
+        }
+
+        @Override
+        public String encodeURL(String s) {
+            return null;
+        }
+
+        @Override
+        public String encodeRedirectURL(String s) {
+            return null;
+        }
+
+        @Override
+        public String encodeUrl(String s) {
+            return null;
+        }
+
+        @Override
+        public String encodeRedirectUrl(String s) {
+            return null;
+        }
+
+        @Override
+        public void sendError(int i, String s) throws IOException {
+
+        }
+
+        @Override
+        public void sendError(int i) throws IOException {
+
+        }
+
+        @Override
+        public void sendRedirect(String s) throws IOException {
+
+        }
+
+        @Override
+        public void setDateHeader(String s, long l) {
+
+        }
+
+        @Override
+        public void addDateHeader(String s, long l) {
+
+        }
+
+        @Override
+        public void setHeader(String s, String s1) {
+
+        }
+
+        @Override
+        public void addHeader(String s, String s1) {
+
+        }
+
+        @Override
+        public void setIntHeader(String s, int i) {
+
+        }
+
+        @Override
+        public void addIntHeader(String s, int i) {
+
+        }
+
+        @Override
+        public void setStatus(int i) {
+
+        }
+
+        @Override
+        public void setStatus(int i, String s) {
+
+        }
+
+        @Override
+        public int getStatus() {
+            return 0;
+        }
+
+        @Override
+        public String getHeader(String s) {
+            return null;
+        }
+
+        @Override
+        public Collection<String> getHeaders(String s) {
+            return null;
+        }
+
+        @Override
+        public Collection<String> getHeaderNames() {
+            return null;
+        }
+
+        @Override
+        public String getCharacterEncoding() {
+            return null;
+        }
+
+        @Override
+        public String getContentType() {
+            return null;
+        }
+
+        @Override
+        public ServletOutputStream getOutputStream() throws IOException {
+            return null;
+        }
+
+        @Override
+        public PrintWriter getWriter() throws IOException {
+            return null;
+        }
+
+        @Override
+        public void setCharacterEncoding(String s) {
+
+        }
+
+        @Override
+        public void setContentLength(int i) {
+
+        }
+
+        @Override
+        public void setContentLengthLong(long l) {
+
+        }
+
+        @Override
+        public void setContentType(String s) {
+
+        }
+
+        @Override
+        public void setBufferSize(int i) {
+
+        }
+
+        @Override
+        public int getBufferSize() {
+            return 0;
+        }
+
+        @Override
+        public void flushBuffer() throws IOException {
+
+        }
+
+        @Override
+        public void resetBuffer() {
+
+        }
+
+        @Override
+        public boolean isCommitted() {
+            return false;
+        }
+
+        @Override
+        public void reset() {
+
+        }
+
+        @Override
+        public void setLocale(Locale locale) {
+
+        }
+
+        @Override
+        public Locale getLocale() {
+            return null;
+        }
+    };
+
+    @MockBean
+    private EmployeeService obj;
+    @Test
+    public void findByNameTest(){
+
+        Map<String,String> result=obj.findByName("endurance",response);
+        Map<String,String> expected_result = new HashMap<String,String>();
+        expected_result.put("username","endurance");
+        expected_result.put("authtoken","jsafdiuewfkj");
+        Assert.assertEquals(result.get("username"),expected_result.get("username"));
+    }
+}
